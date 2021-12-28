268c917d61bd7a38990e5c3894dea9a015f66b46
armdnks <armdn.ks@gmail.com>
2021-12-28 09:00:32 +0700

    main

--- js/main.js
+++ js/main.js
@@ -1,30 +1,23 @@
-// selecting all require elements
-const filterItem = document.querySelector(".items");
-const filterImg = document.querySelectorAll(".image");
+const filterItem = document.querySelector('.items');
+const filterImg = document.querySelectorAll('.image');
 
-// once window loaded
 window.onload = () => {
-  // when user clicked on filter item div
   filterItem.onclick = (selectedItem) => {
-    // if user clicked element has .item class
-    if (selectedItem.target.classList.contains("item")) {
-      // remove active class which is in the first element
-      filterItem.querySelector(".active").classList.remove("active");
-      // add that active class on the user selected element or item
-      selectedItem.target.classList.add("active");
-      // getting data name value of the user selected item
-      let filterName = selectedItem.target.getAttribute("data-name");
+    if (selectedItem.target.classList.contains('item')) {
+      filterItem.querySelector('.active').classList.remove('active');
+
+      selectedItem.target.classList.add('active');
+
+      let filterName = selectedItem.target.getAttribute('data-name');
       filterImg.forEach((image) => {
-        // getting image data value
-        let filterImages = image.getAttribute("data-name");
-        // if user selected item data-name value is equal to image data-name value
-        // or user selected item data-name value is equal to "all"
-        if (filterImages == filterName || filterName == "all") {
-          image.classList.remove("hide");
-          image.classList.add("show");
+        let filterImages = image.getAttribute('data-name');
+
+        if (filterImages == filterName || filterName == 'all') {
+          image.classList.remove('hide');
+          image.classList.add('show');
         } else {
-          image.classList.add("hide");
-          image.classList.remove("show");
+          image.classList.add('hide');
+          image.classList.remove('show');
         }
       });
     }
